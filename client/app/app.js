04656d55570f451b0528318c1748a2cc541f6b98
nickjakubowski <ncjakubowski@gmail.com>
2015-11-06 15:45:34 -0800

    Route request to angular view

--- client/app/app.js
+++ client/app/app.js
@@ -16,7 +16,8 @@ angular.module('shortly', [
       controller: 'AuthController'
     })
     // Your code here
-
+    //without /#/ get request gets sent to server
+    .otherwise('/signup');
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
     $httpProvider.interceptors.push('AttachTokens');
